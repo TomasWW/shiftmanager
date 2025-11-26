055a5c3d3aceeb72c113f97787139fc066b7e4e4
TomasWW <tomas.woelflin@gmail.com>
2025-11-26 20:10:57 +0000

    create model employee and migration

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    GROUP_CHOICES = [
+        ('green','Green'),
+        ('blue', 'Blue')
+    ]
+    
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    work_percentage = models.PositiveIntegerField(default=100)
+    group = models.CharField(max_length=10, choices=GROUP_CHOICES)
+    cycle_index = models.IntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name}, ({self.group})"
+    
+    
